40f59d5ae544615295c777113f4858cc0c26eff3
mikef13x <mikef132044@gmail.com>
2024-06-21 22:02:55 -0400

    created welcome button, which shows up after the intro text is done displaying

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -1,7 +1,14 @@
 import WelcomeCard from "../components/welcomeCard";
+import GetStarted from "../components/getStarted";
+
+// import FakeWelcomeCard from "../components/fakeWelcomeCard";
 
 export default function Homepage() {
+ 
     return (
+        <>
         <WelcomeCard/>
+        <GetStarted/>
+        </>
     )
 }
