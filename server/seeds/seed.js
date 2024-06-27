47c0d4271aa38d9425836ac28b33becee1390a88
Christopher Cuellar <rival100x@gmail.com>
2024-06-27 18:46:10 -0400

    updated typedefs and resolvers began created seeds and seed data

---
+++ server/seeds/seed.js
@@ -0,0 +1,24 @@
+const db = require("../config/connection");
+const { User, Team, Matchup } = require("../models");
+const userSeeds = require("./userSeedData");
+const teamSeeds = require("./teamSeedData");
+const matchupSeeds = require("./matchupSeedData");
+const cleanDB = require("./cleanDB");
+
+db.once("open", async () => {
+  try {
+    await cleanDB();
+
+    await User.insertMany(userSeeds);
+
+    await Team.insertMany(teamSeeds);
+
+    await Matchup.insertMany(matchupSeeds);
+
+    console.log("All seeds successfully inserted!");
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+});
