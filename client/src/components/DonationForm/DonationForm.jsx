3d18a75efca6297f5e41a443c6e19f3d98683009
TheQuestioningCake <perrywayne39@gmail.com>
2024-07-08 18:59:44 -0400

    added stripe payment infortmation and linked to donation

--- client/src/components/DonationForm/DonationForm.jsx
+++ client/src/components/DonationForm/DonationForm.jsx
@@ -1,25 +1,25 @@
 import React, { useState } from 'react';
-import Input from './Input'
-import Button from './Button'
+// import Input from './Input'
+// import Button from './Button'
 
 function DonationForm() {
-    const [donationAmount, setDonationAmount] = useState('')
+    // const [donationAmount, setDonationAmount] = useState('')
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(`Donating ${donationAmount}`);
-    };
+    // const handleSubmit = (e) => {
+    //     e.preventDefault()
+    //     console.log(`Donating ${donationAmount}`);
+    // };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <Input
-                label='Donation Amount'
-                type='number'
-                valut={donationAmount}
-                onChange={(e) => setDonationAmount(e.target.value)}
-            />
-            <Button type='submit' text='Donate'/>
-        </form>
+    // return (
+    //     <form onSubmit={handleSubmit}>
+    //         <Input
+    //             label='Donation Amount'
+    //             type='number'
+    //             valut={donationAmount}
+    //             onChange={(e) => setDonationAmount(e.target.value)}
+    //         />
+    //         <Button type='submit' text='Donate'/>
+    //     </form>
     );
 }
 
