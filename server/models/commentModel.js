b2d1797b9d0dece0381f081399ea15cca56f8eec
Christopher Cuellar <rival100x@gmail.com>
2024-07-09 16:11:09 -0400

    updated comment and reply models

--- server/models/commentModel.js
+++ server/models/commentModel.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose')
+const {replySchema} = require('./replyModel')
 
 const commentSchema = new Schema({
     createdAt: {
--- server/models/replyModel.js
+++ server/models/replyModel.js
@@ -1,30 +1,33 @@
 const { Schema, model } = require('mongoose');
 
 const replySchema = new Schema({
-  content: {
-    type: String,
-    required: true,
-    maxlength: 250
-  },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   username: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  commentId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Comment',
+  },
+  content: {
+    type: String,
+    required: true,
+  },
 }, {
   toJSON: {
     virtuals: true,
   },
-  id: false
-});
-
-replySchema.virtual('formattedDate').get(function () {
-  return this.createdAt.toDateString();
+  id: false,
 });
 
 const Reply = model('Reply', replySchema);
 
-module.exports = Reply;
+module.exports = { Reply, replySchema };
