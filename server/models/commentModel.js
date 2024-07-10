3b28ebc3d80af6ac8388ae54434b87b24c8beb1c
Christopher Cuellar <rival100x@gmail.com>
2024-07-09 20:49:03 -0400

    added DBURI connection string from atlas

--- server/models/commentModel.js
+++ server/models/commentModel.js
@@ -1,27 +1,27 @@
-const { Schema, model } = require('mongoose')
-const {replySchema} = require('./replyModel')
+const { Schema, model } = require('mongoose');
+const { replySchema } = require('./replyModel');
 
 const commentSchema = new Schema({
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    userId: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-    },
-    replies: [replySchema],
-  }, {
-    toJSON: {
-      virtuals: true,
-    },
-    id: false,
-  });
-  
-  const Comment = model('Comment', commentSchema);
-  
-  module.exports = Comment;
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  replies: [replySchema],
+}, {
+  toJSON: {
+    virtuals: true,
+  },
+  id: false,
+});
+
+const Comment = model('Comment', commentSchema);
+
+module.exports = Comment;
